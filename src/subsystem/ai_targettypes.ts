ee9fc0201d7959ecd14356b09903301b235f9a9d
joris <jorisvddonk@gmail.com>
2019-03-20 21:54:42 +0100

    More typesdefinitions

--- src/subsystem/ai_targettypes.ts
+++ src/subsystem/ai_targettypes.ts
@@ -1,6 +1,22 @@
+import Planet from '../planet'
+import Ship from '../ship'
+import Sylvester from '../sylvester-withmods'
+
 export enum TargetType {
   GAMEOBJECT,
   POSITION,
   NULL,
   HALT,
+}
+
+export type TargetF = Ship | Planet | Sylvester.Vector | TargetType.HALT
+
+export class Target {
+  public targetType: TargetType
+  public target: TargetF
+
+  constructor(targetType: TargetType, target: TargetF) {
+    this.target = target
+    this.targetType = targetType
+  }
 }
