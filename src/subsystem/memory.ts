9f3cbfa0cb2ce5a80b14ee2cd0b46bd8a3b1d632
joris <jorisvddonk@gmail.com>
2019-03-23 20:29:30 +0100

    More type definitions

--- src/subsystem/memory.ts
+++ src/subsystem/memory.ts
@@ -5,8 +5,13 @@ export enum MessageType {
   TAKEN_DAMAGE = 'TAKEN_DAMAGE',
 }
 
+export interface IMessage {
+  type: MessageType
+  data: any
+}
+
 export class MemorySubsystem extends ShipSubsystem {
-  private deque: any[]
+  private deque: IMessage[]
   private deque_indexOfLast: number
   private limit: any
   constructor(ship, options) {
@@ -24,25 +29,32 @@ export class MemorySubsystem extends ShipSubsystem {
     this.limit = options.limit
   }
 
-  public push(type: MessageType, data) {
+  public push(type: MessageType, data: any) {
     if (type === undefined || data === undefined) {
       throw new Error('Missing type or data')
     }
-    this.deque.push({ type, data })
+    this.deque.push({ type, data } as IMessage)
     this.deque_indexOfLast += 1
     if (this.deque.length > this.limit) {
       this.removeOldest()
     }
   }
 
-  public read(index, callback) {
+  public read(
+    index: number,
+    callback: (index: number, message: IMessage) => void
+  ) {
     const i = this.deque_indexOfLast - index
     if (i >= 0 && i < this.deque.length) {
       callback(index, this.deque[i])
     }
   }
 
-  public readRange(first, last, callback) {
+  public readRange(
+    first: number,
+    last: number,
+    callback: (index: number, message: IMessage) => void
+  ) {
     _.range(first, last).forEach(index => {
       this.read(index, callback)
     })
