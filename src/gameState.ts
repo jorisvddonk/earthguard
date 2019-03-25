66f85cbe0208594140a895997a2556c1ac89f1b1
joris <jorisvddonk@gmail.com>
2019-03-25 20:10:58 +0100

    Refactor gameState into class

--- src/gameState.ts
+++ src/gameState.ts
@@ -1,37 +1,56 @@
 import EventEmitter from 'EventEmitter'
-const ee = new EventEmitter()
-const gameState = {
-  player: new Proxy(
-    {
-      currentstar: null,
-      ship: null,
-      selectedShip: null,
-    },
-    {
-      set: (obj, prop, value) => {
-        obj[prop] = value
-        if (prop === 'currentstar') {
-          ee.emit('starChanged')
-        }
-        if (prop === 'selectedShip') {
-          ee.emit('shipChanged')
-        }
-        return true
+import { EventEmitter } from 'events'
+
+class GameState {
+  public on: any
+  public debugging: { shiplines: boolean }
+  public universe: { starmap: any }
+  public containers: {
+    solarSystem: any
+    osd: any
+    osd_world: any
+    parallax: any
+  }
+  public player: { currentstar: any; ship: any; selectedShip: any }
+  private ee: EventEmitter
+  constructor() {
+    this.ee = new EventEmitter()
+    this.player = new Proxy(
+      {
+        currentstar: null,
+        ship: null,
+        selectedShip: null,
       },
+      {
+        set: (obj, prop, value) => {
+          obj[prop] = value
+          if (prop === 'currentstar') {
+            this.ee.emit('starChanged')
+          }
+          if (prop === 'selectedShip') {
+            this.ee.emit('shipChanged')
+          }
+          return true
+        },
+      }
+    )
+    this.containers = {
+      solarSystem: null,
+      osd: null,
+      osd_world: null,
+      parallax: null,
     }
-  ),
-  containers: {
-    solarSystem: null,
-    osd: null,
-    osd_world: null,
-    parallax: null,
-  },
-  universe: {
-    starmap: null,
-  },
-  debugging: {
-    shiplines: false,
-  },
-  on: ee.on.bind(ee),
+    this.universe = {
+      starmap: null,
+    }
+    this.debugging = {
+      shiplines: false,
+    }
+
+    this.on = this.ee.on.bind(this.ee)
+  }
 }
+
+const gameState = new GameState()
+
 export default gameState
